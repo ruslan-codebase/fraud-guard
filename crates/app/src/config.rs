edcbef5bd76e6d2c7a16ba38fb959e22650266de
Ruslan Filimonov <ruslan.filimonov.main@mail.ru>
2026-08-27 18:06:06 +0300

    feat: add config, basic main setup

---
+++ crates/app/src/config.rs
@@ -0,0 +1,49 @@
+use serde::Deserialize;
+use std::env;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum ConfigError {
+    #[error("Configuration error: {0}")]
+    Generic(String),
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct Config {
+    pub database_url: String,
+    // pub amqp_url: String,
+    pub log_level: String,
+    // pub rules_refresh_interval_secs: u64,
+}
+
+impl Config {
+    pub fn from_env() -> Result<Self, ConfigError> {
+        let _ = dotenvy::dotenv();
+
+        let database_url = env::var("DATABASE_URL")
+            .map_err(|e| ConfigError::Generic(format!("DATABASE_URL: {}", e)))?;
+
+        // let amqp_url =
+        //     env::var("AMQP_URL").map_err(|e| ConfigError::Generic(format!("AMQP_URL: {}", e)))?;
+
+        let log_level =
+            env::var("RUST_LOG").map_err(|e| ConfigError::Generic(format!("RUST_LOG: {}", e)))?;
+
+        // let rules_refresh_interval_secs = env::var("RULES_REFRESH_INTERVAL_SECS")
+        //     .ok()
+        //     .map(|s| {
+        //         s.parse().map_err(|e| {
+        //             ConfigError::Generic(format!("RULES_REFRESH_INTERVAL_SECS: {}", e))
+        //         })
+        //     })
+        //     .transpose()?
+        //     .unwrap_or(60);
+
+        Ok(Config {
+            database_url,
+            // amqp_url,
+            log_level,
+            // rules_refresh_interval_secs,
+        })
+    }
+}
--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Fraud Guard Service Started");
+mod config;
+
+use config::Config;
+use fraud_guard_domain::RuleRepository;
+use fraud_guard_storage::postgres::{
+    PostgresDecisionRepository, PostgresRuleRepository, PostgresTransactionRepository,
+};
+
+use std::error;
+use std::time;
+use tokio::main;
+use tracing_subscriber::{EnvFilter, fmt};
+
+#[main]
+async fn main() -> Result<(), Box<dyn error::Error>> {
+    let config = Config::from_env()?;
+
+    fmt()
+        .with_env_filter(EnvFilter::new(&config.log_level))
+        .init();
+
+    let pool = sqlx::PgPool::connect(&config.database_url).await?;
+
+    let _ = PostgresTransactionRepository::new(pool.clone());
+    let rule_repo = PostgresRuleRepository::new(pool.clone());
+    let _ = PostgresDecisionRepository::new(pool.clone());
+
+    let rules = rule_repo.load_active_rules().await?;
+    tracing::info!("Loaded {} active rules", rules.len());
+
+    // TODO: setup actor based processing pipeline
+
+    tokio::time::sleep(time::Duration::from_secs(2)).await;
+    Ok(())
 }
